a9b7e8ac3acee2395f5b0a3e786a0e79f560735c
renusree43 <ch.renu2424@gmail.com>
2026-01-08 08:34:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "80px", fontFamily: "Arial" }}>
+        <h1>name-Chakka Renu Sree Charitha</h1>
+        <p>email-2500030373@kluniversity.in</p>
+      </div>
+    );
+  }
+}
+
+export default App;
